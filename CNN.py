444cb1da7a8cb8a0c8c19228822ee6c9d39146c4
George Henry <123128357+hearthoya@users.noreply.github.com>
2023-07-08 15:40:40 -0400

    Add files via upload

--- CNN.py
+++ CNN.py
@@ -2,17 +2,13 @@ import tensorflow as tf
 import os
 import cv2
 import imghdr
-import matplotlib
-from matplotlib import pyplot as plt
-import numpy as np
-from tensorflow import keras
-from keras.metrics import Precision, Recall, BinaryAccuracy
 
 # directory for data
-data_dir = 'data'
+data_dir = "CCSN_v2"
 # array of img extension types
-image_exts = ['jpeg', 'jpg', 'bmp', 'png']
-# remove bad images
+image_exts = ['jpeg', 'jpg']
+
+# Remove bad images
 for image_class in os.listdir(data_dir):
     for image in os.listdir(os.path.join(data_dir, image_class)):
         image_path = os.path.join(data_dir, image_class, image)
@@ -24,60 +20,74 @@ for image_class in os.listdir(data_dir):
                 os.remove(image_path)
         except Exception as e:
             print('Issue with image {}'.format(image_path))
-# load data
-data = tf.keras.utils.image_dataset_from_directory('data')
-data_iterator = data.as_numpy_iterator()
-batch = data_iterator.next()
-# scale data
-data = data.map(lambda x, y: (x / 255, y))
-data.as_numpy_iterator().next()
-# define sizes of each  data partition
-train_size = int(len(data) * .7)
-val_size = int(len(data) * .2) + 1
-test_size = int(len(data) * .1) + 1
-# actually split data up into training, validation and testing
-train = data.take(train_size)
-val = data.skip(train_size).take(val_size)
-test = data.skip(train_size + val_size).take(test_size)
+
+# Load data
+data = tf.keras.utils.image_dataset_from_directory(data_dir, image_size=(256, 256), batch_size=32)
+
+# Define class names
+class_names = data.class_names
+
+# Define sizes of each data partition
+train_size = int(len(data) * 0.7)
+val_size = int(len(data) * 0.2)
+test_size = int(len(data) * 0.1)
+
+# Split data into training, validation, and testing
+train_data = data.take(train_size)
+val_data = data.skip(train_size).take(val_size)
+test_data = data.skip(train_size + val_size).take(test_size)
+
+# Auto define partitions
+AUTOTUNE = tf.data.AUTOTUNE
+train_data = train_data.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_data = val_data.cache().prefetch(buffer_size=AUTOTUNE)
+test_data = test_data.cache().prefetch(buffer_size=AUTOTUNE)
+
 # make model
-model = tf.keras.Sequential()
-# layer 1 and 2
-model.add(tf.keras.layers.Conv2D(16, (3, 3), 1, activation='relu', input_shape=(256, 256, 3)))
-model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-# layer 3 and 4
-model.add(tf.keras.layers.Conv2D(32, (3, 3), 1, activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-# layer 5 and 6
-model.add(tf.keras.layers.Conv2D(16, (3, 3), 1, activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-# layer 7
-model.add(tf.keras.layers.Flatten())
-# layer 8 and 9
-model.add(tf.keras.layers.Dense(256, activation='relu'))
-model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
-# compile
-model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
-# train model
-logdir = 'logs'
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
-hist = model.fit(train, epochs=30, validation_data=val, callbacks=[tensorboard_callback])
-# # plot data
-# fig = plt.figure()
-# plt.plot(hist.history['loss'], color='teal', label='loss')
-# plt.plot(hist.history['val_loss'], color='orange', label='val_loss')
-# fig.suptitle('Loss', fontsize=20)
-# plt.legend(loc="upper left")
-# plt.show()
-# test
-testImg = cv2.imread("cirrustest.jpg")
-testOut = model.predict(np.expand_dims((tf.image.resize(testImg, (256, 256))) / 255, 0))
-if testOut > 0.5:
-    print(f'Test image is Cumulonimbus!')
-else:
-    print(f'Test image is Cirrus!')
-# save model
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(256, 256, 3)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(len(class_names), activation='softmax')
+])
+
+# Compile
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
+
+# Train
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir='logs')
+model.fit(train_data, epochs=40, validation_data=val_data, callbacks=[tensorboard_callback])
+
+# Save=
 model.save(os.path.join('models', 'cloudmodel.keras'))
 
-# load saved model
-# from keras.models import load_model
-# new_model = load_model(os.path.join('models', 'cloudmodel.keras'))
+# Load and preprocess the test image
+test_image_path = "test.jpg"
+test_image = cv2.imread(test_image_path)
+resized_image = cv2.resize(test_image, (256, 256))
+input_image = resized_image / 255.0  # Normalize the image
+
+# Expand dimensions to match the input shape of the model
+input_image = tf.expand_dims(input_image, axis=0)
+
+# Make predictions on the test image
+predictions = model.predict(input_image)
+
+# Get the class index with the highest probability
+predicted_class_index = tf.argmax(predictions, axis=1)[0].numpy()
+
+# Get the class label based on the index
+class_labels = ['Altocumulus', 'Altostratus', 'Circocumulus',
+                'Cirrostratus', 'Cirrus', 'Contrail',
+                'Cumulonimbus', 'Cumulus', 'Nimbostratus',
+                'Stratocumulus', 'Stratus']
+predicted_class_label = class_labels[predicted_class_index]
+
+print("Predicted Class:", predicted_class_label)
